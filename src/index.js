7cef27922105cbbcc566f4d2f1337ee5f908d9ec
Natalia Byzova <nata22011997@gmail.com>
2022-02-21 15:43:44 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let substrArr = [];
+    let result = ''
+    
+    for(i = 0; i < expr.length; i+=10) {
+        substrArr.push(expr.substr(i, 10))
+    }
+
+    
+    let numArr = substrArr.map((item) => parseInt(item, 10).toString())
+
+    let numSubstrArr = numArr.map((item) => {
+        morseStr = '';
+        for(i = 1; i < item.length; i+=2) {
+            if(item[i] === 'a') {
+                morseStr += ' '
+            } else if (item[i] === '0') {
+                morseStr += '.'
+            } else {
+                morseStr += '-'
+            }
+        }
+        return morseStr;
+    })
+
+    for(let item of numSubstrArr) {
+        if (item !== ' ') {
+            result += MORSE_TABLE[item] 
+        } else {
+            result += ' '
+        }
+    }
+    return result
 }
+
+
 
 module.exports = {
     decode
